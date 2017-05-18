46ec81c1b8d6d07de0925af63e8ad052d36285fa
mqliang <mqliang@caicloud.io>
2017-05-18 23:50:53 +0900

    first 4 problems

---
+++ leetcode-go/001-Two-Sum.go
@@ -0,0 +1,12 @@
+package leetcode_go
+
+func twoSum(nums []int, target int) []int {
+	m := map[int]int{}
+	for i := 0; i< len(nums); i++ {
+		if _, exist := m[target-nums[i]]; exist {
+			return []int{i, m[target-nums[i]]}
+		}
+		m[nums[i]] = i
+	}
+	return []int{}
+}
---
+++ leetcode-go/004-Median-of-Two-Sorted-Arrays.go
@@ -0,0 +1,47 @@
+package leetcode_go
+
+const maxint = (^0)>>1
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	sum := len(nums1)+len(nums2)
+	if sum%2 ==1 {
+		return findkth(nums1, nums2, 0, 0, sum/2+1)
+	} else {
+		return (findkth(nums1, nums2, 0, 0, sum/2)+findkth(nums1, nums2, 0, 0, sum/2+1))/2
+	}
+	return 0
+}
+
+func findkth(nums1, nums2 []int, sz1, sz2, k int) float64 {
+	if sz1 >= len(nums1) {
+		return float64(nums2[sz2+k-1])
+	}
+	if sz2 >= len(nums2) {
+		return float64(nums1[sz1+k-1])
+	}
+	if k == 1 {
+		return min(nums1[sz1], nums2[sz2])
+	}
+
+	a, b := maxint, maxint
+	if sz1+k/2-1 < len(nums1) {
+		a = nums1[sz1+k/2-1]
+	}
+	if sz2+k/2-1 < len(nums2) {
+		b = nums2[sz2+k/2-1]
+	}
+
+	if a < b {
+		return findkth(nums1, nums2, sz1+k/2, sz2, k-k/2)
+	} else {
+		return findkth(nums1, nums2, sz1, sz2+k/2, k-k/2)
+	}
+	return 0
+}
+
+func min(a, b int) float64 {
+	if a < b {
+		return float64(a)
+	}
+	return float64(b)
+}
