3d9afc0c9d7283ea1cafbff56cc310568c7050a5
mqliang <mqliang@caicloud.io>
2017-05-25 16:07:53 +0900

    brute force solution for problem 011

--- leetcode-go/011-Container-With-Most-Water.go
+++ leetcode-go/011-Container-With-Most-Water.go
@@ -1,5 +1,19 @@
 package leetcode_go
 
+// solution 1: brute force
+func maxArea(height []int) int {
+	ans := 0
+	for i := 0; i < len(height)-1; i++ {
+		for j := i + 1; j < len(height); j++ {
+			if min(height[j], height[i])*(j-i) > ans {
+				ans = min(height[j], height[i]) * (j - i)
+			}
+		}
+	}
+	return ans
+}
+
+// solution 2: brute force with prune
 func maxArea(height []int) int {
 	l, r := 0, len(height)-1
 	ans := 0
