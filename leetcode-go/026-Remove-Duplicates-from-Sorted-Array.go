0b5633c3ba83ba665acecd8664a1aa0436342b01
mqliang <mqliang@caicloud.io>
2017-05-25 10:30:40 +0900

    problem 026

---
+++ leetcode-go/026-Remove-Duplicates-from-Sorted-Array.go
@@ -0,0 +1,16 @@
+package leetcode_go
+
+func removeDuplicates(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+
+	idx := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[idx] = nums[i]
+			idx++
+		}
+	}
+	return idx
+}
