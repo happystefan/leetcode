a284636053b15d5e0fdc68b65736b0a197bbb963
mqliang <mqliang@caicloud.io>
2017-05-25 10:34:21 +0900

    problem 027

---
+++ leetcode-go/027-Remove-Element.go
@@ -0,0 +1,12 @@
+package leetcode_go
+
+func removeElement(nums []int, val int) int {
+	idx := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[idx] = nums[i]
+			idx++
+		}
+	}
+	return idx + 1
+}
