b4162a0915fe8de49720d360a1e4cebbd25372aa
mqliang <mqliang@caicloud.io>
2017-06-01 19:33:07 +0900

    Q050

---
+++ leetcode-go/Q050_Pow_x_n/Q050_Pow_x_n.go
@@ -0,0 +1,16 @@
+package Q050_Pow_x_n
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+	if n%2 == 0 {
+		return myPow(x*x, n/2)
+	} else {
+		return x * myPow(x*x, n/2)
+	}
+}
