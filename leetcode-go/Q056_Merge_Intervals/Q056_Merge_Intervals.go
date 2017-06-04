99faad48d08629a2a851e248c0aed17ca1f15755
mqliang <mqliang@caicloud.io>
2017-06-04 15:52:28 +0900

    Q056

---
+++ leetcode-go/Q056_Merge_Intervals/Q056_Merge_Intervals.go
@@ -0,0 +1,48 @@
+package Q056_Merge_Intervals
+
+import (
+	"sort"
+)
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+func merge(intervals []Interval) []Interval {
+	sort.Sort(itvs(intervals))
+	ans := []Interval{}
+	for i := 0; i < len(intervals); i++ {
+		itv := intervals[i]
+		for j := i + 1; j < len(intervals) && intervals[j].Start <= itv.End; j++ {
+			itv.End = max(itv.End, intervals[j].End)
+			i++
+		}
+		ans = append(ans, itv)
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type itvs []Interval
+
+func (itv itvs) Less(i, j int) bool {
+	if itv[i].Start == itv[j].Start {
+		return itv[i].End < itv[j].End
+	}
+	return itv[i].Start < itv[j].Start
+}
+
+func (itv itvs) Len() int {
+	return len(itv)
+}
+
+func (itv itvs) Swap(i, j int) {
+	itv[i], itv[j] = itv[j], itv[i]
+}
