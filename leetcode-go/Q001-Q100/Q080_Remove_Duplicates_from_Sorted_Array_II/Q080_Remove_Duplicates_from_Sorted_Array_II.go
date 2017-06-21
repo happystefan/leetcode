2776f7d52f0baecd498b96bc25832cf54f7176f0
mqliang <mqliang@caicloud.io>
2017-06-21 23:37:53 +0900

    Q080

--- leetcode-go/Q001-Q100/Q080_Remove_Duplicates_from_Sorted_Array_II/Q080_Remove_Duplicates_from_Sorted_Array_II.go
+++ leetcode-go/Q001-Q100/Q080_Remove_Duplicates_from_Sorted_Array_II/Q080_Remove_Duplicates_from_Sorted_Array_II.go
@@ -4,11 +4,11 @@ func removeDuplicates(nums []int) int {
 	if len(nums) < 3 {
 		return len(nums)
 	}
-	idx := 1
+	idx := 2
 	for i := 2; i < len(nums); i++ {
-		if !(nums[i] == nums[idx] && nums[i] == nums[idx-1]) {
-			idx++
+		if !(nums[i] == nums[idx-1] && nums[i] == nums[idx-2]) {
 			nums[idx] = nums[i]
+			idx++
 		}
 	}
 	return idx
