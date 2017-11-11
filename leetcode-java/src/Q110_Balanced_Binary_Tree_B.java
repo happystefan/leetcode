d08b4adb3385f713bc301cb5a61eb93b4928dc42
mqliang <mqliang.zju@gmail.com>
2017-11-11 22:02:57 +0900

    Q110

---
+++ leetcode-java/src/Q110_Balanced_Binary_Tree_B.java
@@ -0,0 +1,22 @@
+public class Q110_Balanced_Binary_Tree_B {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+        if (Math.abs(lheight-rheight) > 1 || lheight == -1 || rheight == -1) return -1;
+        return Math.max(lheight, rheight)+1;
+    }
+    
+}
