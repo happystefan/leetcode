97d95926fe1b6789688addbe5eecf27d8dcb5ce7
mqliang <mqliang.zju@gmail.com>
2017-11-13 18:08:23 +0900

    Q125

---
+++ leetcode-java/src/Q125_Valid_Palindrome.java
@@ -0,0 +1,18 @@
+public class Q125_Valid_Palindrome {
+
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+
+        int i = 0, j = s.length()-1;
+        while (true) {
+            while (i < s.length()-1 && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (j > 0 && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if (i > j) break;
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
