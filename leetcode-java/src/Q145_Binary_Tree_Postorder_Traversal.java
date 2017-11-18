e6216696ea06b6cb34952f7e7ab115a8119ecbee
mqliang <mqliang.zju@gmail.com>
2017-11-18 13:28:03 +0900

    Q145

---
+++ leetcode-java/src/Q145_Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q145_Binary_Tree_Postorder_Traversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        dfs(ans, root);
+        return ans;
+    }
+
+    public void dfs(List<Integer> ans, TreeNode root) {
+        if (root == null) return;
+        dfs(ans, root.left);
+        dfs(ans, root.right);
+        ans.add(root.val);
+    }
+
+}
