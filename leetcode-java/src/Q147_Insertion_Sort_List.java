554ada6696ceb52f8161d59342db5559d62ab03e
mqliang <mqliang.zju@gmail.com>
2017-11-18 17:22:16 +0900

    Q147

---
+++ leetcode-java/src/Q147_Insertion_Sort_List.java
@@ -0,0 +1,29 @@
+public class Q147_Insertion_Sort_List {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0);
+        ListNode cur = head;    // current node need to be inserted
+        ListNode nxt = null;    // next node need to be inserted
+
+        while (cur != null) {
+            nxt = cur.next;
+            ListNode pos = dummy; // insert node between pos and pos.next
+            while (pos.next != null && pos.next.val < cur.val) {
+                pos = pos.next;
+            }
+            cur.next = pos.next;
+            pos.next = cur;
+            cur = nxt;
+        }
+        return dummy.next;
+    }
+
+}
