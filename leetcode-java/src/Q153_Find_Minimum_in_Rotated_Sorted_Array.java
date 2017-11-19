c6bca55ddbc5a3909521c272f376a620ffbbdaec
mqliang <mqliang.zju@gmail.com>
2017-11-19 13:35:06 +0900

    Q153

---
+++ leetcode-java/src/Q153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,13 @@
+public class Q153_Find_Minimum_in_Rotated_Sorted_Array {
+
+    public int findMin(int[] nums) {
+        int l = 0, h = nums.length-1;
+        while (l < h) {
+            int m = (l+h)/2;
+            if (nums[m] > nums[h]) l = m+1;
+            else h = m;
+        }
+        return nums[l];
+    }
+
+}
