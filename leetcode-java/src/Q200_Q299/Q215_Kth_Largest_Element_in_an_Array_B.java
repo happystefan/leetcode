924f9ebe073c6f2fa838b5a1f1ab44747495618e
mqliang <mqliang.zju@gmail.com>
2017-11-22 23:31:45 +0900

    Q215

---
+++ leetcode-java/src/Q200_Q299/Q215_Kth_Largest_Element_in_an_Array_B.java
@@ -0,0 +1,14 @@
+package Q200_Q299;
+
+import java.util.PriorityQueue;
+
+public class Q215_Kth_Largest_Element_in_an_Array_B {
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int num : nums) queue.add(num);
+        while(queue.size() > k) queue.poll();
+        return queue.peek();
+    }
+
+}
