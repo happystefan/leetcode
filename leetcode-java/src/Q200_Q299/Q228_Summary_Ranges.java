7f015a35925427c1b3cba8aa103841e77cbd6234
mqliang <mqliang.zju@gmail.com>
2017-11-24 20:46:06 +0900

    Q228

---
+++ leetcode-java/src/Q200_Q299/Q228_Summary_Ranges.java
@@ -0,0 +1,21 @@
+package Q200_Q299;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q228_Summary_Ranges {
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ans = new LinkedList<>();
+        int i = 0, j = 0;
+        while (i < nums.length) {
+            while (j+1 < nums.length && nums[j+1] == nums[j]+1) j++;
+            if (i == j) ans.add(String.valueOf(nums[i]));
+            else ans.add(String.format("%d->%d", nums[i], nums[j]));
+            j++;
+            i = j;
+        }
+        return ans;
+    }
+
+}
