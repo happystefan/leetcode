132aa3f1a914eda1b9036273478ec049a15fd674
mqliang <mqliang.zju@gmail.com>
2017-12-21 13:01:52 +0900

    Q001

---
+++ lintcode-java/src/Q001_A_Plus_B_Problem.java
@@ -0,0 +1,16 @@
+/**
+ * Created by mingqiangliang on 12/21/17.
+ */
+public class Q001_A_Plus_B_Problem {
+
+    public int aplusb(int a, int b) {
+        while (b != 0) {
+            int aa = a^b;
+            int bb = (a&b)<<1;
+            a = aa;
+            b = bb;
+        }
+        return a;
+    }
+
+}
