4db0686abcf647d2c5d9b112e35015f6b8b17532
mqliang <mqliang.zju@gmail.com>
2017-12-21 13:07:50 +0900

    Q002

---
+++ lintcode-java/src/Q002_Trailing_Zeros.java
@@ -0,0 +1,15 @@
+/**
+ * Created by mingqiangliang on 12/21/17.
+ */
+public class Q002_Trailing_Zeros {
+
+    public long trailingZeros(long n) {
+        long ans = 0;
+        while (n != 0) {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+
+}
