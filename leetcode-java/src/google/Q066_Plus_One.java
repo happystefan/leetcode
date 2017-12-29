cdc96e555eac57b245b7d484b2807cb00260a7de
mqliang <mqliang.zju@gmail.com>
2017-12-29 16:47:06 +0900

    Q066

---
+++ leetcode-java/src/google/Q066_Plus_One.java
@@ -0,0 +1,21 @@
+package google;
+
+public class Q066_Plus_One {
+
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for (int i = digits.length-1; i >= 0; i--) {
+            int ncarry = (digits[i]+carry)/10;
+            digits[i] = (digits[i]+carry)%10;
+            carry = ncarry;
+            if (carry == 0) return digits;
+        }
+        int[] ans = new int[digits.length+1];
+        ans[0] = 1;
+        for (int i = 0; i < digits.length; i++) {
+            ans[i+1] = digits[i];
+        }
+        return ans;
+    }
+
+}
