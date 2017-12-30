a7ece16e18b86e59121a956065a0d20a77efe214
mqliang <mqliang.zju@gmail.com>
2017-12-30 18:19:47 +0900

    Q448

---
+++ leetcode-java/src/google/Q448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -0,0 +1,20 @@
+package google;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q448_Find_All_Numbers_Disappeared_in_an_Array {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int v = Math.abs(nums[i])-1;
+            if (nums[v] > 0) nums[v] *= -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) ans.add(i+1);
+        }
+        return ans;
+    }
+
+}
