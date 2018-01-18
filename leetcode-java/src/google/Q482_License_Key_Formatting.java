38be06522aba0bec34784ba73d3cb8bcd357e5a5
mqliang <mqliang.zju@gmail.com>
2018-01-18 11:50:32 -0500

    Q482

---
+++ leetcode-java/src/google/Q482_License_Key_Formatting.java
@@ -0,0 +1,20 @@
+package google;
+
+
+public class Q482_License_Key_Formatting {
+
+    public String licenseKeyFormatting(String S, int K) {
+        S = S.replaceAll("-", "");
+        System.out.println(S);
+        int first = S.length()%K;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (Character.isLetter(c)) c = Character.toUpperCase(c);
+            sb.append(c);
+            if ((i+1-first)%K == 0 && i+1 != S.length()) sb.append('-');
+        }
+        return sb.toString();
+    }
+
+}
---
+++ leetcode-java/src/google/Q482_License_Key_Formatting_B.java
@@ -0,0 +1,15 @@
+package google;
+
+public class Q482_License_Key_Formatting_B {
+
+    public String licenseKeyFormatting(String S, int K) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = S.length()-1; i >= 0; i--) {
+            if (S.charAt(i) == '-') continue;
+            if (sb.length() % (K+1) == K) sb.append('-');
+            sb.append(S.charAt(i));
+        }
+        return sb.reverse().toString().toUpperCase();
+    }
+
+}
