1af198757fdb770e76dbcc20989eab0167b0a7f5
mqliang <mqliang.zju@gmail.com>
2018-04-11 18:14:51 -0400

    Q056

---
+++ lintcode-java/src/facebook/Q056_Two_Sum.java
@@ -0,0 +1,19 @@
+package facebook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q056_Two_Sum {
+
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target-numbers[i])) {
+                return new int[]{map.get(target-numbers[i]), i};
+            }
+            map.put(numbers[i], i);
+        }
+        return null;
+    }
+
+}
