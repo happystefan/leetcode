c7ff24433619a4da83614751f2c8030bab5c13f6
mqliang <mqliang.zju@gmail.com>
2018-04-12 21:24:50 -0400

    Q095

---
+++ lintcode-java/src/facebook/Q095_Validate_Binary_Search_Tree.java
@@ -0,0 +1,28 @@
+package facebook;
+
+public class Q095_Validate_Binary_Search_Tree {
+
+    public class TreeNode {
+        public int val;
+        public TreeNode left, right;
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+
+}
