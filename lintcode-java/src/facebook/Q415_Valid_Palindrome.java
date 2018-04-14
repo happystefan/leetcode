48e5fa83913a4ba48bc1bc8cd82b9a54f8c20592
mqliang <mqliang.zju@gmail.com>
2018-04-14 11:23:07 -0400

    Q415

---
+++ lintcode-java/src/facebook/Q415_Valid_Palindrome.java
@@ -0,0 +1,25 @@
+package facebook;
+
+public class Q415_Valid_Palindrome {
+
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length()-1;
+        while (l < r) {
+            if (!Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+}
