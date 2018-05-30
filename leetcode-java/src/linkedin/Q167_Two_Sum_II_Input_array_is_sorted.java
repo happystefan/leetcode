c69e81311b5cc79972eebe1bafd4f3883d8f6bda
mqliang <mqliang.zju@gmail.com>
2018-05-30 17:11:17 -0400

    Q167

---
+++ leetcode-java/src/linkedin/Q167_Two_Sum_II_Input_array_is_sorted.java
@@ -0,0 +1,20 @@
+package linkedin;
+
+public class Q167_Two_Sum_II_Input_array_is_sorted {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length-1;
+        while (l < r) {
+            int sum = numbers[l]+numbers[r];
+            if (sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            } else {
+                return new int[]{l+1, r+1};
+            }
+        }
+        return null;
+    }
+
+}
