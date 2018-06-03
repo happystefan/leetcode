fc2402bf77aa002b5d9742ee50f854aa13ff4b13
mqliang <mqliang.zju@gmail.com>
2018-06-03 15:59:49 -0400

    Q268

---
+++ leetcode-java/src/linkedin/Q268_Missing_Number_B.java
@@ -0,0 +1,14 @@
+package linkedin;
+
+public class Q268_Missing_Number_B {
+
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += i+1;
+            sum -= nums[i];
+        }
+        return sum;
+    }
+
+}
