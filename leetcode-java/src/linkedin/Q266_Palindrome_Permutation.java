d9a9a32959769d43e9d1f39283b0558c50441a1f
mqliang <mqliang.zju@gmail.com>
2018-06-06 22:10:03 -0400

    Q266

---
+++ leetcode-java/src/linkedin/Q266_Palindrome_Permutation.java
@@ -0,0 +1,22 @@
+package linkedin;
+
+public class Q266_Palindrome_Permutation {
+
+    public boolean canPermutePalindrome(String s) {
+        int[] f = new int[256];
+        for (char c : s.toCharArray()) {
+            f[c]++;
+        }
+        int cnt = 0;
+        for (int i = 0; i < f.length; i++) {
+            if (f[i]%2 == 1) {
+                cnt++;
+            }
+            if (cnt > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
