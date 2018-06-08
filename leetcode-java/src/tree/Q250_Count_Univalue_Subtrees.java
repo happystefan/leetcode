fd575a10d6ba1b4a8f58836926e71d554b93c9e4
mqliang <mqliang.zju@gmail.com>
2018-06-08 14:42:05 -0400

    Q250

---
+++ leetcode-java/src/tree/Q250_Count_Univalue_Subtrees.java
@@ -0,0 +1,65 @@
+package tree;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Q250_Count_Univalue_Subtrees {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(5);
+        root.right.right = new TreeNode(5);
+
+        Solution solution = new Solution();
+        int result = solution.countUnivalSubtrees(root);
+        System.out.println(result);
+    }
+
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+
+
+    public int countUnivalSubtrees(TreeNode root) {
+        Map<TreeNode, Boolean> map = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        map.put(null, true);
+        if (root != null) {
+            stack.push(root);
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            root = stack.peek();
+            if (map.containsKey(root.left) && map.containsKey(root.right)) {
+                map.put(root, (root.left == null || map.get(root.left) && root.val == root.left.val) &&
+                        (root.right == null || map.get(root.right) && root.val == root.right.val));
+                if (map.get(root)) {
+                    result++;
+                }
+                stack.pop();
+            } else {
+                if (root.left != null) {
+                    stack.push(root.left);
+                }
+                if (root.right != null) {
+                    stack.push(root.right);
+                }
+
+            }
+        }
+        return result;
+    }
+
+}
