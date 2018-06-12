01cb3e824734550a84987eac423f16e6e0763283
mqliang <mqliang.zju@gmail.com>
2018-06-11 23:09:51 -0400

    Q112

---
+++ leetcode-java/src/tree/Q112_Path_Sum.java
@@ -0,0 +1,28 @@
+package tree;
+
+public class Q112_Path_Sum {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && sum == root.val) {
+            return true;
+        }
+        if (root.left != null && hasPathSum(root.left, sum-root.val)) {
+            return true;
+        }
+        if (root.right != null && hasPathSum(root.right, sum-root.val)) {
+            return true;
+        }
+        return false;
+    }
+
+}
