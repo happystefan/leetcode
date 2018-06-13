f7092b46618d6b8add88af5baaae3cd359ef849a
mqliang <mqliang.zju@gmail.com>
2018-06-12 23:10:02 -0400

    Q285

---
+++ leetcode-java/src/tree/Q285_Inorder_Successor_in_BST_B.java
@@ -0,0 +1,23 @@
+package tree;
+
+public class Q285_Inorder_Successor_in_BST_B {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null) {
+            return null;
+        }
+        if (p.val >= root.val) {
+            return inorderSuccessor(root.right, p);
+        }
+        TreeNode l = inorderSuccessor(root.left, p);
+        return l == null ? root : l;
+    }
+
+}
