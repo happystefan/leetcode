34168435a5cd7751d7c74800ca1ccd8993ce20d4
mqliang <mqliang.zju@gmail.com>
2018-06-12 21:09:47 -0400

    Q814

---
+++ leetcode-java/src/tree/Q814_Binary_Tree_Pruning_B.java
@@ -0,0 +1,24 @@
+package tree;
+
+public class Q814_Binary_Tree_Pruning_B {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right= pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+
+}
