543d314a6d9da6ab0f841d8834ac9fe164ecff82
mqliang <mqliang.zju@gmail.com>
2018-06-14 01:43:06 -0400

    Q144

---
+++ leetcode-java/src/tree/Q144_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,30 @@
+package tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q144_Binary_Tree_Preorder_Traversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        dfs(result, root);
+        return result;
+    }
+
+    private void dfs(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        dfs(result, root.left);
+        dfs(result, root.right);
+    }
+}
+
---
+++ leetcode-java/src/tree/Q144_Binary_Tree_Preorder_Traversal_B.java
@@ -0,0 +1,35 @@
+package tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Q144_Binary_Tree_Preorder_Traversal_B {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> result = new LinkedList<>();
+        if (root != null) {
+            stack.push(root);
+        }
+        while (!stack.isEmpty()) {
+            TreeNode c = stack.pop();
+            result.add(c.val);
+            if (c.right!= null) {
+                stack.push(c.right);
+            }
+            if (c.left != null) {
+                stack.push(c.left);
+            }
+        }
+        return result;
+    }
+
+}
