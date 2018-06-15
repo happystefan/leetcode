7f71555fd9dd5b416548ac0fcbe93f43524bf438
mqliang <mqliang.zju@gmail.com>
2018-06-15 17:35:05 -0400

    Q230

---
+++ leetcode-java/src/tree/Q230_Kth_Smallest_Element_in_a_BST.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class Q230_Kth_Smallest_Element_in_a_BST {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    int cnt = 0;
+    int result = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        dfs(root, k);
+        return result;
+    }
+
+    private void dfs(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left, k);
+        cnt++;
+        if (cnt == k) {
+            result = root.val;
+            return;
+        }
+        dfs(root.right, k);
+    }
+
+}
