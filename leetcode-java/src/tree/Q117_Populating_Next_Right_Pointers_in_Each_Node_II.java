7f4f1d1a470ecd772ea6aae5dfe992216d13cb83
mqliang <mqliang.zju@gmail.com>
2018-06-15 16:25:39 -0400

    Q117

---
+++ leetcode-java/src/tree/Q117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class Q117_Populating_Next_Right_Pointers_in_Each_Node_II {
+
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+        TreeLinkNode(int x) { val = x; }
+    }
+
+    public void connect(TreeLinkNode root) {
+        while (root != null) {
+            TreeLinkNode dum = new TreeLinkNode(0);
+            TreeLinkNode cur = dum;
+            while (root != null) {
+                if (root.left != null) {
+                    cur.next = root.left;
+                    cur = cur.next;
+                }
+                if (root.right != null) {
+                    cur.next = root.right;
+                    cur = cur.next;
+                }
+                root = root.next;
+            }
+            root = dum.next;
+        }
+
+        return;
+    }
+
+}
