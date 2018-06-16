eacb8dfe87435884304a14d3f258dc8148c4b0e9
mqliang <mqliang.zju@gmail.com>
2018-06-15 22:25:30 -0400

    Q235

---
+++ leetcode-java/src/tree/Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,25 @@
+package tree;
+
+public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int min = Math.min(p.val, q.val), max = Math.max(p.val, q.val);
+        while (root.val < min || root.val > max) {
+            if (root.val < min) {
+                root = root.right;
+            }
+            if (root.val > max) {
+                root = root.left;
+            }
+        }
+        return root;
+    }
+
+}
