2ffd396954c8fc1c98c62c7d3baf042085af4802
mqliang <mqliang.zju@gmail.com>
2018-06-15 21:35:42 -0400

    Q236

---
+++ leetcode-java/src/tree/Q236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class Q236_Lowest_Common_Ancestor_of_a_Binary_Tree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right,p, q);
+        if (l != null && r != null) {
+            return root;
+        } else if (l != null) {
+            return l;
+        } else if (r != null) {
+            return r;
+        } else {
+            return null;
+        }
+    }
+
+}
