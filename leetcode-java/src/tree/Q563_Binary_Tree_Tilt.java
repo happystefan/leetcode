2c684b2e325d0aae4d94d89e599b22eb7e944eda
mqliang <mqliang.zju@gmail.com>
2018-06-19 01:42:56 -0400

    Q563

---
+++ leetcode-java/src/tree/Q563_Binary_Tree_Tilt.java
@@ -0,0 +1,28 @@
+package tree;
+
+public class Q563_Binary_Tree_Tilt {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int findTilt(TreeNode root) {
+        int[] result = new int[1];
+        dfs(result, root);
+        return result[0];
+    }
+
+    private int dfs(int[] result, TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = dfs(result, root.left);
+        int r = dfs(result, root.right);
+        result[0] += Math.abs(l-r);
+        return l+r+root.val;
+    }
+
+}
