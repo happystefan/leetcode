0d192c91b0e1a6b299758841ae020c35eaf4bec3
mqliang <mqliang.zju@gmail.com>
2018-06-20 12:03:57 -0400

    Q654

---
+++ leetcode-java/src/tree/Q654_Maximum_Binary_Tree.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class Q654_Maximum_Binary_Tree {
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return construct(nums, 0, nums.length-1);
+    }
+
+    private TreeNode construct(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        if (l == r) {
+            return new TreeNode(nums[l]);
+        }
+        int max = nums[l], idx = l;
+        for (int i = l; i <= r; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                idx = i;
+            }
+        }
+        TreeNode root = new TreeNode(nums[idx]);
+        root.left = construct(nums, l, idx-1);
+        root.right= construct(nums, idx+1, r);
+        return root;
+    }
+
+}
