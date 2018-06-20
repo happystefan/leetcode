5218eceb7521aa27484cd312b06baac401e36ee8
mqliang <mqliang.zju@gmail.com>
2018-06-19 20:19:54 -0400

    Q606

---
+++ leetcode-java/src/tree/Q606_Construct_String_from_Binary_Tree.java
@@ -0,0 +1,27 @@
+package tree;
+
+public class Q606_Construct_String_from_Binary_Tree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public String tree2str(TreeNode t) {
+        if (t == null) {
+            return "";
+        }
+        String result = String.valueOf(t.val);
+        if (t.left == null && t.right == null) {
+            return result;
+        }
+        result += "(" + tree2str(t.left) + ")";
+        if (t.right!= null) {
+            result += "(" + tree2str(t.right) + ")";
+        }
+        return result;
+    }
+
+}
