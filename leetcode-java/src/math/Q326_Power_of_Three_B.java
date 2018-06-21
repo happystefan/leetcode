5ddf3f68d8ad214c33f6554249c815be1a2075a0
mqliang <mqliang.zju@gmail.com>
2018-06-21 00:29:08 -0400

    Q326

---
+++ leetcode-java/src/math/Q326_Power_of_Three_B.java
@@ -0,0 +1,10 @@
+package math;
+
+public class Q326_Power_of_Three_B {
+    public boolean isPowerOfThree(int n) {
+        if (n == 0) {
+            return false;
+        }
+        return 1162261467%n == 0;
+    }
+}
