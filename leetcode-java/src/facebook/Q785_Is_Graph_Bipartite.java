51be94fd033a7e8326b3007e9c6ff501cee9245b
mqliang <mqliang.zju@gmail.com>
2018-06-24 21:56:15 -0400

    Q785

---
+++ leetcode-java/src/facebook/Q785_Is_Graph_Bipartite.java
@@ -0,0 +1,34 @@
+package facebook;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Q785_Is_Graph_Bipartite {
+
+    public boolean isBipartite(int[][] graph) {
+        int[] color = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] != 0) {
+                continue;
+            }
+            Queue<Integer> Q = new LinkedList<>();
+            Q.offer(i);
+            color[i] = 1;
+            while (!Q.isEmpty()) {
+                int u = Q.poll();
+                for (int v : graph[u]) {
+                    if (color[v] == 0) {
+                        color[v] = 3-color[u];
+                        Q.offer(v);
+                    } else {
+                        if (color[v] != 3-color[u]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+}
