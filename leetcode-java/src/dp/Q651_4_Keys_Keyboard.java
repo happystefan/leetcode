4345c8fa5c8be5cbf5bc598f378bbadae6829144
mqliang <mqliang.zju@gmail.com>
2018-07-06 13:10:32 -0400

    Q651

---
+++ leetcode-java/src/dp/Q651_4_Keys_Keyboard.java
@@ -0,0 +1,17 @@
+package dp;
+
+public class Q651_4_Keys_Keyboard {
+
+    public int maxA(int N) {
+        int[] dp = new int[N+1];
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1]+1;
+            for (int j = 0; j < i-1; j++) {
+                dp[i] = Math.max(dp[i], dp[j]*(i-j-1));
+            }
+        }
+        return dp[N];
+    }
+
+}
