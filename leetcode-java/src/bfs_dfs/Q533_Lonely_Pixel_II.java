1b02bcecf04788db966bdb081430a4571cbf01e3
mqliang <mqliang.zju@gmail.com>
2018-07-08 16:50:04 -0400

    Q533

---
+++ leetcode-java/src/bfs_dfs/Q533_Lonely_Pixel_II.java
@@ -0,0 +1,38 @@
+package bfs_dfs;
+
+import java.util.*;
+
+public class Q533_Lonely_Pixel_II {
+
+    public int findBlackPixel(char[][] picture, int N) {
+        int m = picture.length, n = picture[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        Set<String>[] rows_table = new Set[n];
+        for (int i = 0; i < n; i++) {
+            rows_table[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] == 'B') {
+                    row[i] += 1;
+                    col[j] += 1;
+                    rows_table[j].add(new String(picture[i]));
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] == 'B' && row[i] == N && col[j] == N && rows_table[j].size() == 1) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+}
