fa34bc7c7ff4d7783d75f8970f16deca26091039
mqliang <mqliang.zju@gmail.com>
2018-07-17 19:27:13 -0400

    Q727

---
+++ leetcode-java/src/range_query/Q727_Minimum_Window_Subsequence.java
@@ -0,0 +1,35 @@
+package range_query;
+
+public class Q727_Minimum_Window_Subsequence {
+
+    // Time Limit Exceeded
+    public String minWindow(String S, String T) {
+        int l = 0, r = 0;
+        String result = "";
+        while (l < S.length()) {
+            if (!isSubsequence(S, l, r, T) && r < S.length()) {
+                r++;
+            } else {
+                l++;
+            }
+            if (isSubsequence(S, l, r, T) && (result.length() == 0 || r-l < result.length())) {
+                result = S.substring(l, r);
+            }
+        }
+        return result;
+    }
+
+    private boolean isSubsequence(String S, int l, int r, String T) {
+        int j = 0;
+        for (int i = l; i < r; i++) {
+            if (S.charAt(i) == T.charAt(j)) {
+                j++;
+            }
+            if (j == T.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
