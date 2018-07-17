5ab99aac996d0b3cfc444a18d37bea95f8cef501
mqliang <mqliang.zju@gmail.com>
2018-07-17 12:10:01 -0400

    combination

--- leetcode-java/src/bfs_dfs/Q039_Combination_Sum.java
+++ leetcode-java/src/bfs_dfs/Q039_Combination_Sum.java
@@ -2,11 +2,11 @@ package bfs_dfs;
 
 import java.util.*;
 
-public class Q040_Combination_Sum_II {
+public class Q039_Combination_Sum {
 
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(candidates);
         dfs(result, new LinkedList<>(), candidates, 0, target);
         return result;
     }
@@ -14,18 +14,15 @@ public class Q040_Combination_Sum_II {
     private void dfs(List<List<Integer>> result, List<Integer> list, int[] nums, int pos, int target) {
         if (target == 0) {
             result.add(new LinkedList<>(list));
-            return;
         }
         if (target < 0) {
             return;
         }
         for (int i = pos; i < nums.length; i++) {
-            if (i > pos && nums[i] == nums[i-1]) {
-                continue;
-            }
             list.add(nums[i]);
-            dfs(result, list, nums, i+1, target-nums[i]);
+            dfs(result, list, nums, i, target-nums[i]);
             list.remove(list.size()-1);
+            while (i+1 < nums.length && nums[i+1] == nums[i]) i++;
         }
     }
 
