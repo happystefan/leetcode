fa4f7bd1368021432b0e585974ce64579942d169
mqliang <mqliang.zju@gmail.com>
2018-07-22 20:17:29 -0400

    RangeQuery

---
+++ leetcode-java/src/range_query/Q084_Largest_Rectangle_in_Histogram.java
@@ -0,0 +1,37 @@
+package range_query;
+
+public class Q084_Largest_Rectangle_in_Histogram {
+
+    public int largestRectangleArea(int[] heights) {
+        return largestArea(heights, 0, heights.length-1);
+    }
+
+    private int largestArea(int[] heights, int l, int r) {
+        if (l > r) {
+            return 0;
+        }
+        if (l == r) {
+            return heights[l];
+        }
+        int m = l+(r-l)/2;
+        int i = m, j = m+1;
+        int result = 0, h = Math.min(heights[i], heights[j]);
+        while (i >= l && j <= r) {
+            h = Math.min(h, Math.min(heights[i], heights[j]));
+            result = Math.max(result, h*(j-i+1));
+            if (i == l) {
+                j++;
+            } else if (j == r) {
+                i--;
+            } else {
+                if (heights[i-1] > heights[j+1]) {
+                    i--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return Math.max(result, Math.max(largestArea(heights, l, m), largestArea(heights, m+1, r)));
+    }
+
+}
