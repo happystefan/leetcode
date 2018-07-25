0217befa4ab299f32a87ba6abf962971d9e0b96f
mqliang <mqliang.zju@gmail.com>
2018-07-25 11:43:48 -0400

    facebook

---
+++ leetcode-java/src/facebook/Q246_Strobogrammatic_Number.java
@@ -0,0 +1,29 @@
+package facebook;
+
+import java.util.*;
+
+public class Q246_Strobogrammatic_Number {
+
+    public boolean isStrobogrammatic(String num) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('0', '0');
+        map.put('1', '1');
+        map.put('6', '9');
+        map.put('8', '8');
+        map.put('9', '6');
+        int l = 0, r = num.length()-1;
+        while (l <= r) {
+            if (l == r) {
+                return num.charAt(l) == '0' || num.charAt(l) == '1' || num.charAt(l) == '8';
+            } else {
+                if (!map.containsKey(num.charAt(l)) || map.get(num.charAt(l)) != num.charAt(r)) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ leetcode-java/src/facebook/Q247_Strobogrammatic_Number_II.java
@@ -0,0 +1,31 @@
+package facebook;
+
+import java.util.*;
+
+public class Q247_Strobogrammatic_Number_II {
+
+    private int[][] pairs = new int[][]{{0, 0}, {1, 1}, {6, 9}, {8, 8}, {9, 6}};
+    public List<String> findStrobogrammatic(int n) {
+        List<String> result = new LinkedList<>();
+        if (n%2 == 1) {
+            for (int i : Arrays.asList(0, 1, 8)) {
+                dfs(result, ""+i, n-1);
+            }
+        } else {
+            dfs(result, "", n);
+        }
+        return result;
+    }
+
+    private void dfs(List<String> result, String num, int n) {
+        if (n == 0) {
+            result.add(num);
+            return;
+        }
+        for (int i = n == 2 ? 1 : 0; i < pairs.length; i++) {
+            dfs(result, pairs[i][0]+num+pairs[i][1], n-2);
+        }
+        return;
+    }
+
+}
