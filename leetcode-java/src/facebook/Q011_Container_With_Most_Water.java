a46fb357b13ae08e7b6d41d9201b0b409e65384c
mqliang <mqliang.zju@gmail.com>
2018-07-31 15:26:38 -0400

    misc

---
+++ leetcode-java/src/facebook/Q011_Container_With_Most_Water.java
@@ -0,0 +1,20 @@
+package facebook;
+
+
+public class Q011_Container_With_Most_Water {
+
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length-1;
+        int result = 0;
+        while (l < r) {
+            result = Math.max(result, Math.min(height[l], height[r])*(r-l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return result;
+    }
+
+}
