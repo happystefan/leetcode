ffbd68fde0a87922392535fe97b19314e768e01d
mqliang <mqliang.zju@gmail.com>
2018-08-13 16:14:12 -0400

    Q553

---
+++ leetcode-java/src/google/Q553_Optimal_Division_C.java
@@ -0,0 +1,28 @@
+package google;
+
+public class Q553_Optimal_Division_C {
+
+    public String optimalDivision(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+        if (nums.length == 1) {
+            return String.valueOf(nums[0]);
+        }
+        if (nums.length == 2) {
+            return String.format("%d/%d", nums[0], nums[1]);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(nums[0]);
+        sb.append("/");
+        sb.append("(");
+        sb.append(nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            sb.append("/");
+            sb.append(nums[i]);
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+
+}
