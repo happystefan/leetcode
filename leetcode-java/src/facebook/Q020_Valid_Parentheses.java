0f2acebc9e990d5c6b06f511f4544946c7ff7b84
mqliang <mqliang.zju@gmail.com>
2018-08-15 18:22:06 -0400

    Q020

---
+++ leetcode-java/src/facebook/Q020_Valid_Parentheses.java
@@ -0,0 +1,26 @@
+package facebook;
+
+import java.util.*;
+
+public class Q020_Valid_Parentheses {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c :  s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (!stack.isEmpty() && isPair(stack.peek(), c)) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isPair(char a, char b) {
+        String ab = ""+a+b;
+        return ab.equals("()") || ab.equals("[]") || ab.equals("{}");
+    }
+
+}
