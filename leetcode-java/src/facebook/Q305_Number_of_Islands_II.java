39b3d817cf42c60688d04d07cd4cbd44f04207a7
mqliang <mqliang.zju@gmail.com>
2018-08-17 13:23:05 -0400

    sync

---
+++ leetcode-java/src/facebook/Q305_Number_of_Islands_II.java
@@ -0,0 +1,42 @@
+package facebook;
+
+import java.util.*;
+
+public class Q305_Number_of_Islands_II {
+
+    int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        int cnt = 0;
+        int[] p = new int[m*n];
+        Arrays.fill(p, -1);
+        List<Integer> result = new LinkedList<>();
+        for (int[] position : positions) {
+            int i = position[0], j = position[1];
+            p[i*n+j] = i*n+j;
+            cnt++;
+            int cp = findp(i*n+j, p);
+            for (int[] dir : dirs) {
+                int ii = i+dir[0], jj = j+dir[1];
+                if (ii < 0 || ii >= m || jj < 0 || jj >= n || p[ii*n+jj] == -1) {
+                    continue;
+                }
+                int np = findp(ii*n+jj, p);
+                if (cp != np) {
+                    p[cp] = np;
+                    cp = np;
+                    cnt--;
+                }
+            }
+            result.add(cnt);
+        }
+        return result;
+    }
+
+    private int findp(int i, int[] p) {
+        if (i != p[i]) {
+            p[i] = findp(p[i], p);
+        }
+        return p[i];
+    }
+
+}
