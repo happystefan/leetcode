7b7df5b1025b5f700969723a2319754bfae87266
mqliang <mqliang.zju@gmail.com>
2018-08-26 18:49:38 -0400

    syn

---
+++ leetcode-java/src/facebook/sliding_window/Q424_Longest_Repeating_Character_Replacement.java
@@ -0,0 +1,25 @@
+package facebook.sliding_window;
+
+import java.util.*;
+
+public class Q424_Longest_Repeating_Character_Replacement {
+
+    public int characterReplacement(String s, int k) {
+        char[] ss = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        int l = 0, r = 0, result = 0;
+        int max = 0;
+        while (r < ss.length) {
+            map.put(ss[r], map.getOrDefault(ss[r], 0)+1);
+            max = Math.max(max, map.get(ss[r]));
+            r++;
+            while ((r-l) > max+k) {
+                map.put(ss[l], map.get(ss[l])-1);
+                l++;
+            }
+            result = Math.max(result, r-l);
+        }
+        return result;
+    }
+
+}
