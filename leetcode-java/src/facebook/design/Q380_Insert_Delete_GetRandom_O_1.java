08005b6c2e8afd4c9b43cdc6ffc1f2d53f3e9b7b
mqliang <mqliang.zju@gmail.com>
2018-08-27 15:37:35 -0400

    sync

---
+++ leetcode-java/src/facebook/design/Q380_Insert_Delete_GetRandom_O_1.java
@@ -0,0 +1,53 @@
+package facebook.design;
+
+import java.util.*;
+
+public class Q380_Insert_Delete_GetRandom_O_1 {
+
+    class RandomizedSet {
+
+        /** Initialize your data structure here. */
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        public RandomizedSet() {
+
+        }
+
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public boolean insert(int val) {
+            if (map.containsKey(val)) return false;
+            map.put(val, map.size());
+            list.add(val);
+            return true;
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public boolean remove(int val) {
+            if (!map.containsKey(val)) return false;
+            int idx = map.get(val);
+            if (idx != map.size()-1) {
+                map.put(list.get(list.size()-1), idx);
+                list.set(idx, list.get(list.size()-1));
+            }
+            list.remove(list.size()-1);
+            map.remove(val);
+            return true;
+        }
+
+        /** Get a random element from the set. */
+        public int getRandom() {
+            return list.get(random.nextInt(map.size()));
+        }
+    }
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+
+}
