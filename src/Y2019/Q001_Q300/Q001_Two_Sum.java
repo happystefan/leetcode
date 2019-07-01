d032f8f0ece5b39591bc32c0f98fc10c1ab30858
happystefan <stefan.mqliang@gmail.com>
2019-07-01 13:46:34 -0700

    Q001

---
+++ src/Y2019/Q001_Q300/Q001_Two_Sum.java
@@ -0,0 +1,17 @@
+package Y2019.Q001_Q300;
+
+import java.util.HashMap;
+
+public class Q001_Two_Sum {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
