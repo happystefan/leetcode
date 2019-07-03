4f89b5510a0d610f7c1bd53bc18cb7d5713c7c38
happystefan <stefan.mqliang@gmail.com>
2019-07-02 21:14:01 -0700

    Q004_Remove_An_Edge_To_Recover_Binary_Search_Tree.java

---
+++ src/Y2019/misc/Q004_Remove_An_Edge_To_Recover_Binary_Search_Tree.java
@@ -0,0 +1,33 @@
+package Y2019.misc;
+
+public class Q004_Remove_An_Edge_To_Recover_Binary_Search_Tree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public void removeAnEdgeToRecoverBinarySearchTree(TreeNode root) {
+        dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private void dfs(TreeNode root, long min, long max) {
+        if (root == null) return;
+        if (root.left != null) {
+            if (root.left.val <= min || root.right.val >= max) {
+                root.left = null;
+                return;
+            }
+            dfs(root.left, min, root.val);
+        }
+        if (root.right != null) {
+            if (root.right.val <= min || root.right.val >= max) {
+                root.right = null;
+                return;
+            }
+            dfs(root.right, root.val, max);
+        }
+    }
+
+}
