87881a98be1cb4a0e583788b7c64688f308c654b
happystefan <stefan.mqliang@gmail.com>
2019-07-04 14:35:55 -0700

    Q003_Longest_Substring_Without_Repeating_Characters

---
+++ src/Y2019/Q001_Q300/Q003_Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,23 @@
+package Y2019.Q001_Q300;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q003_Longest_Substring_Without_Repeating_Characters {
+
+    public int lengthOfLongestSubstring(String s) {
+        char[] ss = s.toCharArray();
+        int l = 0, r = 0;
+        Set<Character> set = new HashSet<>();
+        int result = 0;
+        while (r < ss.length) {
+            while (set.contains(ss[r])) {
+                set.remove(ss[l++]);
+            }
+            set.add(ss[r++]);
+            result = Math.max(result, r-l);
+        }
+        return result;
+    }
+
+}
