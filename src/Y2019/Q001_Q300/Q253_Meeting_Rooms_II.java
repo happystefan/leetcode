810aff60c859587848866ced100ed183cfcfb742
happystefan <stefan.mqliang@gmail.com>
2019-07-05 21:25:21 -0700

    Q253_Meeting_Rooms_II

---
+++ src/Y2019/Q001_Q300/Q253_Meeting_Rooms_II.java
@@ -0,0 +1,25 @@
+package Y2019.Q001_Q300;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Q253_Meeting_Rooms_II {
+
+    public int minMeetingRooms(int[][] intervals) {
+        List<int[]> list = new ArrayList<>();
+        for (int[] interval : intervals) {
+            list.add(new int[]{interval[0], 1});
+            list.add(new int[]{interval[1], -1});
+        }
+        Collections.sort(list, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        int result = 0;
+        int sum = 0;
+        for (int[] pair : list) {
+            sum += pair[1];
+            result = Math.max(result, sum);
+        }
+        return result;
+    }
+
+}
