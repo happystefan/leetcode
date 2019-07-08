66b4fd582c28d6e0a16ff0c69041ae3ef1da7b5a
mqliang <secret.mqliang@gmail.com>
2019-07-07 21:56:52 -0700

    Q0021_Merge_Two_Sorted_Lists

---
+++ src/Y2019/Q0001_Q0300/Q0021_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,32 @@
+package Y2019.Q0001_Q0300;
+
+public class Q0021_Merge_Two_Sorted_Lists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 != null) curr.next = l1;
+        if (l2 != null) curr.next = l2;
+        return dummy.next;
+    }
+
+}
