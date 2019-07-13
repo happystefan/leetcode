b12dd8972a49e30eabcd19999776789345383b6e
happystefan <stefan.mqliang@gmail.com>
2019-07-12 19:43:37 -0700

    Q0041_First_Missing_Positive

---
+++ src/Y2019/Q0001_Q0300/Q0041_First_Missing_Positive.java
@@ -0,0 +1,23 @@
+package Y2019.Q0001_Q0300;
+
+public class Q0041_First_Missing_Positive {
+
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        nums[i] ^= nums[j];
+        nums[j] ^= nums[i];
+        nums[i] ^= nums[j];
+    }
+
+}
