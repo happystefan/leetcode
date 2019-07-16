881d981c7f8b5fd8cde7775f01ee52e53d72ae00
happystefan <stefan.mqliang@gmail.com>
2019-07-15 20:28:31 -0700

    Q0081_Search_in_Rotated_Sorted_Array_II

---
+++ src/Y2019/Q0001_Q0300/Q0081_Search_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,25 @@
+package Y2019.Q0001_Q0300;
+
+public class Q0081_Search_in_Rotated_Sorted_Array_II {
+
+    public boolean search_solution1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return true;
+            if (nums[l] < nums[m]) {
+                if (target >= nums[l] && target < nums[m]) r = m - 1;
+                else l = m + 1;
+            } else if (nums[m] < nums[r]) {
+                if (target > nums[m] && target <= nums[r]) l = m + 1;
+                else r = m - 1;
+            } else {
+                if (nums[l] == target) return true;
+                l++;
+            }
+        }
+        return nums[l] == target;
+    }
+
+}
