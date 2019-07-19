d1ba39a44aeaf70ed6d66deeb1eed332390f52a8
happystefan <stefan.mqliang@gmail.com>
2019-07-18 21:19:27 -0700

    Q0077_Combinations

---
+++ src/Y2019/Q0001_Q0300/Q0077_Combinations.java
@@ -0,0 +1,26 @@
+package Y2019.Q0001_Q0300;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q0077_Combinations {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new LinkedList<>();
+        dfs(result, new LinkedList<>(), 1, n, k);
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, List<Integer> list, int pos, int n, int k) {
+        if (list.size() == k) {
+            result.add(new LinkedList<>(list));
+            return;
+        }
+        for (int i = pos; i <= n; i++) {
+            list.add(i);
+            dfs(result, list, i + 1, n, k);
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
