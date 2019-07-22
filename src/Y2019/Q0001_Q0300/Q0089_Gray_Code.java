7ef721234236f605d17b0033eb7e3cb58311f8f0
happystefan <stefan.mqliang@gmail.com>
2019-07-21 20:09:03 -0700

    Q0089_Gray_Code

---
+++ src/Y2019/Q0001_Q0300/Q0089_Gray_Code.java
@@ -0,0 +1,18 @@
+package Y2019.Q0001_Q0300;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q0089_Gray_Code {
+
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>(2 << n);
+        result.add(0);
+        for (int i = 0; i < n; i++) {
+            int size = result.size();
+            for (int j = size - 1; j >= 0; j--) result.add(result.get(j) | (1 << i));
+        }
+        return result;
+    }
+
+}
