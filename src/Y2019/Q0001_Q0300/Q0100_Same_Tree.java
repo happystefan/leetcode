2d4a498ddf2a58f3bdbe0b708fc16ef920bf91df
happystefan <stefan.mqliang@gmail.com>
2019-07-22 19:46:27 -0700

    Q0100_Same_Tree

---
+++ src/Y2019/Q0001_Q0300/Q0100_Same_Tree.java
@@ -0,0 +1,20 @@
+package Y2019.Q0001_Q0300;
+
+public class Q0100_Same_Tree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) return p == q;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
