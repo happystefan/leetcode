6cc890a474631ef746bcd4a03cf69b246b385a00
happystefan <stefan.mqliang@gmail.com>
2019-07-24 21:32:00 -0700

    Q0099_Recover_Binary_Search_Tree

--- src/Y2019/Q0001_Q0300/Q0099_Recover_Binary_Search_Tree.java
+++ src/Y2019/Q0001_Q0300/Q0099_Recover_Binary_Search_Tree.java
@@ -2,11 +2,18 @@ package Y2019.Q0001_Q0300;
 
 public class Q0099_Recover_Binary_Search_Tree {
 
+    /*
+     *------------------------------------------------------------------------------------------------------------------
+     *
+     * solution 1: dfs
+     *
+     *------------------------------------------------------------------------------------------------------------------
+     */
     TreeNode x = null;
     TreeNode y = null;
     TreeNode p = null;
 
-    public void recoverTree(TreeNode root) {
+    public void recoverTree_dfs(TreeNode root) {
         dfs(root);
         int t = x.val;
         x.val = y.val;
@@ -35,6 +42,52 @@ public class Q0099_Recover_Binary_Search_Tree {
         TreeNode(int x) {
             val = x;
         }
+    }
+
+    /*
+     *------------------------------------------------------------------------------------------------------------------
+     *
+     * solution 2: morris traversal
+     *
+     *------------------------------------------------------------------------------------------------------------------
+     */
+    public void recoverTree(TreeNode root) {
+        TreeNode x = null;
+        TreeNode y = null;
+        TreeNode p = null;
+        TreeNode pred = null;
+        while (root != null) {
+            if (root.left == null) {
+                {
+                    if (p != null && p.val > root.val) {
+                        y = root;
+                        if (x == null) x = p;
+                    }
+                    p = root;
+                }
+                root = root.right;
+            } else {
+                pred = root.left;
+                while (pred.right != null && pred.right != root) pred = pred.right;
+                if (pred.right == null) {
+                    pred.right = root;
+                    root = root.left;
+                } else {
+                    {
+                        if (p != null && p.val > root.val) {
+                            y = root;
+                            if (x == null) x = p;
+                        }
+                        p = root;
+                    }
+                    pred.right = null;
+                    root = root.right;
+                }
+            }
+        }
+        int t = x.val;
+        x.val = y.val;
+        y.val = t;
     }
 
 }
