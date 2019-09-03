231e49cefda779fffafa25f54fbdb41650133c5f
happystefan <stefan.mqliang@gmail.com>
2019-09-02 18:14:39 -0700

    Q0516_Longest_Palindromic_Subsequence.java

---
+++ src/Y2019/facebook/Q0516_Longest_Palindromic_Subsequence.java
@@ -0,0 +1,27 @@
+package Y2019.facebook;
+
+public class Q0516_Longest_Palindromic_Subsequence {
+
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        char[] ss = s.toCharArray();
+        for (int i = 0; i + 1 < n; i++) {
+            if (ss[i] == ss[i + 1]) dp[i][i + 1] = 2;
+            else dp[i][i + 1] = 1;
+        }
+        for (int l = 2; l < n; l++) {
+            for (int i = 0; i + l < n; i++) {
+                int j = i + l;
+                if (ss[i] == ss[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
+                else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+}
