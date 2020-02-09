b657b389cf74ff077177e264feeeb53b1c6c6626
mqliang <secret.mqliang@gmail.com>
2020-02-09 15:10:38 -0800

    Q0167_Two_Sum_II_Input_array_is_sorted

---
+++ src/Y2020/facebook/Q0167_Two_Sum_II_Input_array_is_sorted.java
@@ -0,0 +1,19 @@
+package Y2020.facebook;
+
+public class Q0167_Two_Sum_II_Input_array_is_sorted {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            if (numbers[i] + numbers[j] > target) {
+                j--;
+            } else if (numbers[i] + numbers[j] < target) {
+                i++;
+            } else {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+        return null;
+    }
+
+}
